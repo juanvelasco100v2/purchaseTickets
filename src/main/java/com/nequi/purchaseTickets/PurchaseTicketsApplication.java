08888a3ae603e349b613ad450ca332ac9d16973d
juanmanuelvelasco <juanvelasco100@gmail.com>
2026-02-12 21:20:51 -0500

    Initial commit

---
+++ src/main/java/com/nequi/purchaseTickets/PurchaseTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.purchaseTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseTicketsApplication {
+//inicio app
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseTicketsApplication.class, args);
+	}
+//fin app
+}
